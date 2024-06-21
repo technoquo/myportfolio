2bb8f69ea8ffa4bd253d88719e5e32a873f0787d
Leonel Lopez <technoquo@gmail.com>
2024-06-21 17:10:00 +0200

    poner color dark para el  menu movil

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -229,7 +229,7 @@
             </div> --}}
         </div>
 
-        <div class="items-center justify-between hidden w-full lg:flex md:w-auto md:order-1 dark:bg-gray-900"
+        <div class="items-center justify-between hidden w-full lg:flex md:w-auto md:order-1 dark:bg-gray-700 py-3 px-4 rounded"
             id="navbar-language">
             <ul
                 class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
