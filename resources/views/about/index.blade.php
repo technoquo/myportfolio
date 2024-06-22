92ff9154e523eadca2847fb89ada66bfda48fc7a
Leonel Lopez <technoquo@gmail.com>
2024-06-22 11:51:16 +0200

    cambiar color svg para descargar

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -137,14 +137,14 @@
                                                     <g id="SVGRepo_tracerCarrier" stroke-linecap="round"
                                                         stroke-linejoin="round"></g>
                                                     <g id="SVGRepo_iconCarrier">
-                                                        <path d="M12 7L12 14M12 14L15 11M12 14L9 11" stroke="#1C274C"
+                                                        <path d="M12 7L12 14M12 14L15 11M12 14L9 11" stroke="#FFFFFF"
                                                             stroke-width="1.5" stroke-linecap="round"
                                                             stroke-linejoin="round"></path>
-                                                        <path d="M16 17H12H8" stroke="#1C274C" stroke-width="1.5"
+                                                        <path d="M16 17H12H8" stroke="#FFFFFF" stroke-width="1.5"
                                                             stroke-linecap="round"></path>
                                                         <path
                                                             d="M2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C22 4.92893 22 7.28595 22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12Z"
-                                                            stroke="#1C274C" stroke-width="1.5"></path>
+                                                            stroke="#FFFFFF" stroke-width="1.5"></path>
                                                     </g>
                                                 </svg>
                                             </a>
@@ -173,14 +173,14 @@
                                                     <g id="SVGRepo_tracerCarrier" stroke-linecap="round"
                                                         stroke-linejoin="round"></g>
                                                     <g id="SVGRepo_iconCarrier">
-                                                        <path d="M12 7L12 14M12 14L15 11M12 14L9 11" stroke="#1C274C"
+                                                        <path d="M12 7L12 14M12 14L15 11M12 14L9 11" stroke="#FFFFFF"
                                                             stroke-width="1.5" stroke-linecap="round"
                                                             stroke-linejoin="round"></path>
-                                                        <path d="M16 17H12H8" stroke="#1C274C" stroke-width="1.5"
+                                                        <path d="M16 17H12H8" stroke="#FFFFFF" stroke-width="1.5"
                                                             stroke-linecap="round"></path>
                                                         <path
                                                             d="M2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C22 4.92893 22 7.28595 22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12Z"
-                                                            stroke="#1C274C" stroke-width="1.5"></path>
+                                                            stroke="#FFFFFF" stroke-width="1.5"></path>
                                                     </g>
                                                 </svg>
                                             </a>
@@ -261,14 +261,14 @@
                                                     <g id="SVGRepo_tracerCarrier" stroke-linecap="round"
                                                         stroke-linejoin="round"></g>
                                                     <g id="SVGRepo_iconCarrier">
-                                                        <path d="M12 7L12 14M12 14L15 11M12 14L9 11" stroke="#1C274C"
+                                                        <path d="M12 7L12 14M12 14L15 11M12 14L9 11" stroke="#FFFF"
                                                             stroke-width="1.5" stroke-linecap="round"
                                                             stroke-linejoin="round"></path>
-                                                        <path d="M16 17H12H8" stroke="#1C274C" stroke-width="1.5"
+                                                        <path d="M16 17H12H8" stroke="#FFFF" stroke-width="1.5"
                                                             stroke-linecap="round"></path>
                                                         <path
                                                             d="M2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C22 4.92893 22 7.28595 22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12Z"
-                                                            stroke="#1C274C" stroke-width="1.5"></path>
+                                                            stroke="#FFFF" stroke-width="1.5"></path>
                                                     </g>
                                                 </svg>
                                             </a>
@@ -302,14 +302,14 @@
                                                     <g id="SVGRepo_tracerCarrier" stroke-linecap="round"
                                                         stroke-linejoin="round"></g>
                                                     <g id="SVGRepo_iconCarrier">
-                                                        <path d="M12 7L12 14M12 14L15 11M12 14L9 11" stroke="#1C274C"
+                                                        <path d="M12 7L12 14M12 14L15 11M12 14L9 11" stroke="#FFFF"
                                                             stroke-width="1.5" stroke-linecap="round"
                                                             stroke-linejoin="round"></path>
-                                                        <path d="M16 17H12H8" stroke="#1C274C" stroke-width="1.5"
+                                                        <path d="M16 17H12H8" stroke="#FFFF" stroke-width="1.5"
                                                             stroke-linecap="round"></path>
                                                         <path
                                                             d="M2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C22 4.92893 22 7.28595 22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12Z"
-                                                            stroke="#1C274C" stroke-width="1.5"></path>
+                                                            stroke="#FFFF" stroke-width="1.5"></path>
                                                     </g>
                                                 </svg>
                                             </a>
