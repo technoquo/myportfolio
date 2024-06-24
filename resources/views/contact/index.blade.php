1fe3d5b873dc45f5c004474ce01327678323beac
Leonel Lopez <technoquo@gmail.com>
2024-06-24 11:38:07 +0200

    agregado fiverr

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -102,6 +102,12 @@
                                 </g>
                             </svg>
                         </div>
+                        <div class="mb-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="150" height="150" viewBox="0 0 24 24">
+                                <path fill="white"
+                                    d="M23.004 15.588a.995.995 0 1 0 .002-1.99a.995.995 0 0 0-.002 1.99m-.996-3.705h-.85c-.546 0-.84.41-.84 1.092v2.466h-1.61v-3.558h-.684c-.547 0-.84.41-.84 1.092v2.466h-1.61v-4.874h1.61v.74c.264-.574.626-.74 1.163-.74h1.972v.74c.264-.574.625-.74 1.162-.74h.527zm-6.786 1.501h-3.359c.088.546.43.858 1.006.858c.43 0 .732-.175.83-.487l1.425.4c-.351.848-1.22 1.364-2.255 1.364c-1.748 0-2.549-1.355-2.549-2.515c0-1.14.703-2.505 2.45-2.505c1.856 0 2.471 1.384 2.471 2.408c0 .224-.01.37-.02.477zm-1.562-.945c-.04-.42-.342-.81-.889-.81c-.508 0-.81.225-.908.81zM7.508 15.44h1.416l1.767-4.874h-1.62l-.86 2.837l-.878-2.837H5.72l1.787 4.874zm-6.6 0H2.51v-3.558h1.524v3.558h1.591v-4.874H2.51v-.302c0-.332.235-.536.606-.536h.918V8.412H2.85c-1.162 0-1.943.712-1.943 1.755v.4H0v1.316h.908v3.558z" />
+                            </svg>
+                        </div>
                     </div>
 
                 </div>
@@ -123,7 +129,7 @@
 
                         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Linkeldn
                         </h5>
-                        <p class="font-normal text-gray-700 dark:text-gray-400">linkedin.com/in/lelobo</p>
+                        <p class="font-normal text-gray-700 dark:text-gray-400">lelobo</p>
                     </a>
 
 
@@ -146,6 +152,13 @@
                         <p class="font-normal text-gray-700 dark:text-gray-400">@lelobo75</p>
                     </a>
 
+                    <a href="https://www.fiverr.com/lelobo" target="_blank"
+                        class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 mb-3">
+
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Fiverr
+                        </h5>
+                        <p class="font-normal text-gray-700 dark:text-gray-400">lelobo</p>
+                    </a>
 
 
 
